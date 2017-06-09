f9882a994cda49f6e110abbc6c1b3dfe4d030cd9
Ajusa Jain <arhamjain@gmail.com>
2017-06-08 21:56:03 -0400

    0.1
    Game is basically done, and presentable. Next thing to do would be to
    add story, music, and dialog

--- main.lua
+++ main.lua
@@ -3,6 +3,7 @@ local Camera = require("camera")
 local bump = require('bump')
 love.graphics.setDefaultFilter("nearest")
 love.graphics.setFont(love.graphics.newFont("kenpixel.ttf", 14))
+love.window.setFullscreen(true)
 local random
 random = function(l, h)
   return love.math.random(l, h)
@@ -11,7 +12,24 @@ local collision
 collision = function(x1, y1, w1, h1, x2, y2, w2, h2)
   return x1 < x2 + w2 and x2 < x1 + w1 and y1 < y2 + h2 and y2 < y1 + h1
 end
+local playerFilter
+playerFilter = function(item, other)
+  if other.p ~= nil and other.p.isEnemy then
+    return "cross"
+  else
+    return "slide"
+  end
+end
+local enemyFilter
+enemyFilter = function(item, other)
+  if other.p ~= nil and (other.p.isEnemy or other.p.speed == 100) then
+    return false
+  else
+    return "slide"
+  end
+end
 local sinceFire = 0
+local score = 0
 local Entity
 do
   local _class_0
@@ -98,19 +116,21 @@ do
       return love.graphics.draw(enemy, self.p.x, self.p.y, 0, 4, 4)
     end,
     update = function(self, dt, i)
-      local angle = math.atan2((player.y - self.p.y), (player.x - self.p.x))
+      self.p.speed = self.p.speed + dt
+      local angle = math.atan2((player.p.y - self.p.y), (player.p.x - self.p.x))
       self.p.dx, self.p.dy = self.p.speed * math.cos(angle), self.p.speed * math.sin(angle)
       self.p.x, self.p.y = world:move(self, self.p.x + self.p.dx * dt, self.p.y + self.p.dy * dt, enemyFilter)
       for i = #bullets, 1, -1 do
-        local v = bullets[i]
-        if collision(v.p.x, v.p.y, 20, 40, self.p.x, self.p.y, 64, 64) then
+        if collision(bullets[i].p.x, bullets[i].p.y, 20, 40, self.p.x, self.p.y, 64, 64) then
           self.p.lives = self.p.lives - 1
+          score = score + 1
           table.remove(bullets, i)
         end
       end
       if self.p.lives < 1 then
         do
           world:remove(self)
+          score = score + 5
           return table.remove(enemies, i)
         end
       end
@@ -150,46 +170,80 @@ do
   end
   Enemy = _class_0
 end
-player = {
-  x = 100,
-  y = 100,
-  w = 64,
-  h = 64,
-  speed = 100,
-  lives = 5
-}
-local camera = Camera(player.x, player.y)
-local playerFilter
-playerFilter = function(item, other)
-  if other.p ~= nil and other.p.isEnemy then
-    other.p.lives = 0
-    item.lives = item.lives - 1
-    return "cross"
+local Player
+do
+  local _class_0
+  local _parent_0 = Entity
+  local _base_0 = {
+    draw = function(self)
+      return love.graphics.draw(self.p.image, self.p.x, self.p.y, 0, 4, 4)
+    end,
+    update = function(self, dt)
+      for i = #enemies, 1, -1 do
+        if collision(enemies[i].p.x + 8, enemies[i].p.y + 8, 48, 48, self.p.x, self.p.y, 64, 64) then
+          self.p.lives = self.p.lives - 1
+          table.remove(enemies, i)
+        end
+      end
+    end
+  }
+  _base_0.__index = _base_0
+  setmetatable(_base_0, _parent_0.__base)
+  _class_0 = setmetatable({
+    __init = function(self, p)
+      self.p = p
+      return world:add(self, self.p.x, self.p.y, 64, 64)
+    end,
+    __base = _base_0,
+    __name = "Player",
+    __parent = _parent_0
+  }, {
+    __index = function(cls, name)
+      local val = rawget(_base_0, name)
+      if val == nil then
+        local parent = rawget(cls, "__parent")
+        if parent then
+          return parent[name]
+        end
+      else
+        return val
+      end
+    end,
+    __call = function(cls, ...)
+      local _self_0 = setmetatable({}, _base_0)
+      cls.__init(_self_0, ...)
+      return _self_0
+    end
+  })
+  _base_0.__class = _class_0
+  if _parent_0.__inherited then
+    _parent_0.__inherited(_parent_0, _class_0)
   end
-  return "slide"
-end
-enemyFilter = function(item, other)
-  if other.p ~= nil and other.p.isEnemy then
-    return false
-  else
-    return "slide"
-  end
+  Player = _class_0
 end
 love.load = function()
   world = bump.newWorld()
   map = sti("data/testmap.lua", {
     "bump"
   })
-  player.image = love.graphics.newImage("player.png")
+  player = Player({
+    x = 43 * 64,
+    y = 6 * 64,
+    w = 64,
+    h = 64,
+    speed = 100,
+    lives = 5,
+    image = love.graphics.newImage("player.png")
+  })
+  camera = Camera(player.p.x, player.p.y)
   map:bump_init(world)
-  world:add(player, player.x, player.y, 64, 64)
   do
     local _accum_0 = { }
     local _len_0 = 1
     for i = 1, 40 do
       _accum_0[_len_0] = Enemy({
-        x = random(256, (map.width - 2) * 64),
-        y = random(256, (map.height - 2) * 64),
+        x = random(0, (32) * 64),
+        y = random(0, (map.height - 2) * 64),
         lives = 5,
         isEnemy = true,
         speed = 30
@@ -204,58 +258,68 @@ love.load = function()
   enemy = love.graphics.newImage("enemy.png")
 end
 love.update = function(dt)
-  map:update(dt)
-  sinceFire = sinceFire + dt
-  if love.keyboard.isDown("a") then
-    player.x, player.y = world:move(player, player.x - player.speed * dt, player.y, playerFilter)
+  if player.p.lives > 0 then
+    map:update(dt)
+    sinceFire = sinceFire + dt
+    if love.keyboard.isDown("a") then
+      player.p.x, player.p.y = world:move(player, player.p.x - player.p.speed * dt, player.p.y, playerFilter)
+    end
+    if love.keyboard.isDown("d") then
+      player.p.x, player.p.y = world:move(player, player.p.x + player.p.speed * dt, player.p.y, playerFilter)
+    end
+    if love.keyboard.isDown("w") then
+      player.p.x, player.p.y = world:move(player, player.p.x, player.p.y - player.p.speed * dt, playerFilter)
+    end
+    if love.keyboard.isDown("s") then
+      player.p.x, player.p.y = world:move(player, player.p.x, player.p.y + player.p.speed * dt, playerFilter)
+    end
+    for i = #bullets, 1, -1 do
+      bullets[i]:update(dt, i)
+    end
+    for i = #enemies, 1, -1 do
+      enemies[i]:update(dt, i)
+    end
+    player:update(dt)
+    return camera:lockPosition(player.p.x, player.p.y)
   end
-  if love.keyboard.isDown("d") then
-    player.x, player.y = world:move(player, player.x + player.speed * dt, player.y, playerFilter)
-  end
-  if love.keyboard.isDown("w") then
-    player.x, player.y = world:move(player, player.x, player.y - player.speed * dt, playerFilter)
-  end
-  if love.keyboard.isDown("s") then
-    player.x, player.y = world:move(player, player.x, player.y + player.speed * dt, playerFilter)
-  end
-  for i = #bullets, 1, -1 do
-    bullets[i]:update(dt, i)
-  end
-  for i = #enemies, 1, -1 do
-    enemies[i]:update(dt, i)
-  end
-  return camera:lockPosition(player.x, player.y)
 end
 love.draw = function()
-  camera:attach()
-  map:draw()
-  local sx, sy = camera:worldCoords(12, 12)
-  love.graphics.print(player.lives, sx, sy)
-  for i, v in ipairs(bullets) do
-    v:draw()
+  if player.p.lives > 0 then
+    camera:attach()
+    map:draw()
+    for i, v in ipairs(bullets) do
+      v:draw()
+    end
+    for i, v in ipairs(enemies) do
+      v:draw()
+    end
+    player:draw()
+    camera:detach()
+    love.graphics.setColor(255, 0, 0, score)
+    love.graphics.rectangle("fill", 0, 0, love.graphics.getWidth(), love.graphics.getHeight())
+    love.graphics.setColor(255, 255, 255, 255)
+    love.graphics.print("Lives: " .. player.p.lives, 12, 12)
+    return love.graphics.print("Score: " .. score, 100, 12)
+  else
+    love.graphics.setFont(love.graphics.newFont("kenpixel.ttf", 30))
+    love.graphics.print("GAME OVER MACBETH!", love.graphics.getWidth() / 4, love.graphics.getHeight() / 2)
+    return love.graphics.print("Score: " .. score, love.graphics.getWidth() / 4, love.graphics.getHeight() / 1.5)
   end
-  for i, v in ipairs(enemies) do
-    v:draw()
-  end
-  love.graphics.draw(player.image, player.x, player.y, 0, 4, 4)
-  return camera:detach()
 end
 love.mousepressed = function(x, y, button)
   if button == 1 and sinceFire > .3 then
     sinceFire = 0
-    local startX = player.x + 32
-    local startY = player.y + 32
+    local startX, startY = player.p.x + 32, player.p.y + 32
     local mouseX, mouseY = camera:worldCoords(x, y)
     local angle = math.atan2((mouseY - startY), (mouseX - startX))
     local dx, dy = bulletSpeed * math.cos(angle), bulletSpeed * math.sin(angle)
-    local bullet = Dagger({
+    return table.insert(bullets, Dagger({
       x = startX,
       y = startY,
       dx = dx,
       dy = dy,
       angle = angle,
       distance = 0
-    })
-    return table.insert(bullets, bullet)
+    }))
   end
 end
--- main.moon
+++ main.moon
@@ -3,9 +3,13 @@ Camera = require "camera"
 bump = require 'bump'
 love.graphics.setDefaultFilter("nearest")
 love.graphics.setFont(love.graphics.newFont("kenpixel.ttf", 14))
+love.window.setFullscreen(true)
 random = (l, h) -> love.math.random(l, h)
 collision = (x1,y1,w1,h1, x2,y2,w2,h2) -> x1 < x2+w2 and x2 < x1+w1 and y1 < y2+h2 and y2 < y1+h1
+playerFilter = (item, other) -> if other.p != nil and other.p.isEnemy then "cross" else "slide"
+enemyFilter = (item, other) -> if other.p != nil and (other.p.isEnemy or other.p.speed == 100) then false else "slide"
 sinceFire = 0
+score = 0
 class Entity
   new: (p) => @p = p
   update: (dt) => @p.x, @p.y = @p.x + @p.dx*dt, @p.y + @p.dy*dt
@@ -21,68 +25,74 @@ class Enemy extends Entity
 		world\add(self, @p.x, @p.y, 64, 64)
 	draw: => love.graphics.draw(enemy, @p.x, @p.y, 0,4,4)
 	update: (dt, i) =>
-		angle = math.atan2((player.y - @p.y), (player.x - @p.x))
+		@p.speed += dt
+		angle = math.atan2((player.p.y - @p.y), (player.p.x - @p.x))
 		@p.dx, @p.dy  = @p.speed * math.cos(angle), @p.speed * math.sin(angle)
 		@p.x, @p.y = world\move(self,@p.x + @p.dx*dt, @p.y + @p.dy*dt, enemyFilter)
-		for i=#bullets,1,-1 do 
-			v = bullets[i]
-			if collision(v.p.x, v.p.y, 20, 40, @p.x, @p.y, 64, 64) then
+		for i=#bullets,1,-1 do if collision(bullets[i].p.x, bullets[i].p.y, 20, 40, @p.x, @p.y, 64, 64) then
 				@p.lives -= 1
+				score += 1
 				table.remove(bullets, i)
 		if @p.lives < 1 do 
 				world\remove(self)
+				score += 5
 				table.remove(enemies, i)
-
-export player = x: 100, y: 100, w: 64, h: 64, speed: 100, lives: 5
-camera = Camera(player.x, player.y)
-playerFilter = (item, other) -> 
-	if other.p != nil and other.p.isEnemy
-		other.p.lives = 0
-		item.lives -= 1
-		return "cross"
-	return "slide"
-export enemyFilter = (item, other) -> if other.p != nil and other.p.isEnemy then false else "slide"
+class Player extends Entity
+	new: (p) => 
+		@p = p
+		world\add(self, @p.x, @p.y, 64, 64)
+	draw: => love.graphics.draw(@p.image, @p.x, @p.y, 0,4,4)
+	update: (dt) =>
+		for i=#enemies,1,-1 do if collision(enemies[i].p.x + 8, enemies[i].p.y + 8, 48, 48, @p.x, @p.y, 64, 64) -- the 8's are for a smaller hitbox
+				@p.lives -= 1
+				table.remove(enemies, i)
 love.load = ->	
 	export world = bump.newWorld!
 	export map = sti("data/testmap.lua", {"bump"})
-	player.image = love.graphics.newImage("player.png")
+	export player = Player x: 43*64, y: 6*64, w: 64, h: 64, speed: 100, lives: 5, image: love.graphics.newImage("player.png")
+	export camera = Camera(player.p.x, player.p.y)
 	map\bump_init(world)
-	world\add(player, player.x, player.y, 64, 64)
-	export enemies = for i = 1, 40 do Enemy x: random(256, (map.width-2)*64), y: random(256, (map.height-2)*64), lives: 5, isEnemy: true, speed: 30
+	export enemies = for i = 1, 40 do Enemy x: random(0, (32)*64), y: random(0, (map.height-2)*64), lives: 5, isEnemy: true, speed: 30
 	export bulletSpeed = 250
 	export bullets = {}
 	export dagger = love.graphics.newImage("dagger.png")
 	export enemy = love.graphics.newImage("enemy.png")
 love.update = (dt) ->
+	if player.p.lives > 0
 		-- Update world
-	map\update(dt)
-	sinceFire += dt
-	if love.keyboard.isDown("a") then player.x, player.y = world\move(player, player.x - player.speed*dt,player.y, playerFilter)
-	if love.keyboard.isDown("d") then player.x, player.y = world\move(player, player.x + player.speed*dt,player.y, playerFilter)
-	if love.keyboard.isDown("w") then player.x, player.y = world\move(player, player.x, player.y - player.speed*dt, playerFilter)
-	if love.keyboard.isDown("s") then player.x, player.y = world\move(player, player.x, player.y + player.speed*dt, playerFilter)
-	for i=#bullets,1,-1 do bullets[i]\update(dt,i)
-	for i=#enemies,1,-1 do enemies[i]\update(dt,i)
-	camera\lockPosition(player.x, player.y)
+		map\update(dt)
+		sinceFire += dt
+		if love.keyboard.isDown("a") then player.p.x, player.p.y = world\move(player, player.p.x - player.p.speed*dt,player.p.y, playerFilter)
+		if love.keyboard.isDown("d") then player.p.x, player.p.y = world\move(player, player.p.x + player.p.speed*dt,player.p.y, playerFilter)
+		if love.keyboard.isDown("w") then player.p.x, player.p.y = world\move(player, player.p.x, player.p.y - player.p.speed*dt, playerFilter)
+		if love.keyboard.isDown("s") then player.p.x, player.p.y = world\move(player, player.p.x, player.p.y + player.p.speed*dt, playerFilter)
+		for i=#bullets,1,-1 do bullets[i]\update(dt,i)
+		for i=#enemies,1,-1 do enemies[i]\update(dt,i)
+		player\update(dt)
+		camera\lockPosition(player.p.x, player.p.y)
 love.draw = ->
-	camera\attach()
-	map\draw()
-	sx, sy = camera\worldCoords(12,12)
-	love.graphics.print(player.lives, sx, sy)
-	for i,v in ipairs(bullets) do v\draw!
-	for i,v in ipairs(enemies) do v\draw!
-	love.graphics.draw(player.image, player.x, player.y, 0, 4, 4)
-	--map\bump_draw(world) --this is the debug code for seeing collision boxes
-	camera\detach()
-
+	if player.p.lives > 0
+		camera\attach()
+		map\draw()
+		for i,v in ipairs(bullets) do v\draw!
+		for i,v in ipairs(enemies) do v\draw!
+		player\draw!
+		--map\bump_draw(world) --this is the debug code for seeing collision boxes
+		camera\detach()
+		love.graphics.setColor(255, 0, 0, score)
+		love.graphics.rectangle("fill", 0, 0, love.graphics.getWidth!, love.graphics.getHeight!)
+		love.graphics.setColor(255, 255, 255, 255)
+		love.graphics.print("Lives: "..player.p.lives, 12, 12)
+		love.graphics.print("Score: "..score, 100, 12)
+	else 
+		love.graphics.setFont(love.graphics.newFont("kenpixel.ttf", 30))
+		love.graphics.print("GAME OVER MACBETH!", love.graphics.getWidth!/4, love.graphics.getHeight!/2)
+		love.graphics.print("Score: ".. score, love.graphics.getWidth!/4, love.graphics.getHeight!/1.5)
 love.mousepressed = (x, y, button) ->
 	if button == 1 and sinceFire > .3
 		sinceFire = 0
-		startX = player.x + 32
-		startY = player.y + 32
+		startX, startY = player.p.x + 32, player.p.y + 32
 		mouseX, mouseY = camera\worldCoords(x,y) --this stops the mouse coords from being off from the real coors, cause we have a camera
 		angle = math.atan2((mouseY - startY), (mouseX - startX))
-		dx, dy  = bulletSpeed * math.cos(angle), bulletSpeed * math.sin(angle)
-		bullet = Dagger x: startX, y: startY, :dx, :dy, :angle, distance: 0
-		table.insert(bullets, bullet)
-		--world\add(bullet, bullet.p.x, bullet.p.y, 20, 40)
+		dx, dy = bulletSpeed * math.cos(angle), bulletSpeed * math.sin(angle)
+		table.insert(bullets, Dagger x: startX, y: startY, :dx, :dy, :angle, distance: 0)
